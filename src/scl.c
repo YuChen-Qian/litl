8de8a24b0f3ac306ca35655f6e9130bc7026f3f7
YuChen Qian <yuchenqian96@gmail.com>
2021-02-20 16:24:28 +0100

    fixup! scl: add scl lock

--- src/scl.c
+++ src/scl.c
@@ -48,29 +48,25 @@ static inline int futex(int *uaddr, int futex_op, int val,
     return syscall(SYS_futex, uaddr, futex_op, val, timeout, NULL, 0);
 }
 
-static inline scl_node_t *flqnode(scl_mutex_t *lock) {
-    return (scl_node_t *)((char *)&lock->next - offsetof(scl_node_t, next));
-}
-
 scl_mutex_t *scl_mutex_create(const pthread_mutexattr_t *attr) {
-    scl_mutex_t *impl = (scl_mutex_t *)alloc_cache_align(sizeof(scl_mutex_t));
-    if (!impl) {
+    scl_mutex_t *lock = (scl_mutex_t *)alloc_cache_align(sizeof(scl_mutex_t));
+    if (!lock) {
         return NULL;
     }
 
-    impl->tail         = NULL;
-    impl->next         = NULL;
-    impl->total_weight = 0;
-    impl->slice        = 0;
-    impl->slice_valid  = 0;
-    if (pthread_key_create(&impl->tinfo_key, NULL)) {
+    lock->tail         = NULL;
+    lock->next         = NULL;
+    lock->total_weight = 0;
+    lock->slice        = 0;
+    lock->slice_valid  = 0;
+    if (pthread_key_create(&lock->tinfo_key, NULL)) {
         return NULL;
     }
 
-    return impl;
+    return lock;
 }
 
-static tinfo_t *tinfo_create(scl_mutex_t *impl, int weight) {
+static tinfo_t *tinfo_create(scl_mutex_t *lock, int weight) {
     tinfo_t *info = alloc_cache_align(sizeof(tinfo_t));
     if (!info) {
         return NULL;
@@ -84,7 +80,7 @@ static tinfo_t *tinfo_create(scl_mutex_t *impl, int weight) {
         weight = prio_to_weight[prio + 20];
     }
     info->weight = weight;
-    __sync_add_and_fetch(&impl->total_weight, weight);
+    __sync_add_and_fetch(&lock->total_weight, weight);
     info->banned      = 0;
     info->slice       = 0;
     info->start_ticks = 0;
@@ -95,12 +91,12 @@ static tinfo_t *tinfo_create(scl_mutex_t *impl, int weight) {
     return info;
 }
 
-static tinfo_t *__thread_info(scl_mutex_t *impl) {
+static tinfo_t *__thread_info(scl_mutex_t *lock) {
     // Retrieve per-thread tracking information
-    tinfo_t *info = (tinfo_t *)pthread_getspecific(impl->tinfo_key);
+    tinfo_t *info = (tinfo_t *)pthread_getspecific(lock->tinfo_key);
     if (!info) {
-        info = tinfo_create(impl, 0);
-        pthread_setspecific(impl->tinfo_key, info);
+        info = tinfo_create(lock, 0);
+        pthread_setspecific(lock->tinfo_key, info);
     }
     return info;
 }
@@ -113,8 +109,9 @@ static void __scl_mutex_lock_reenter(struct tinfo *info,
     info->start_ticks = now;
 }
 
-static int __scl_mutex_lock_begin(scl_mutex_t *impl, tinfo_t *info,
-                                  unsigned long long now) {
+static int __scl_mutex_lock(scl_mutex_t *lock, tinfo_t *info) {
+    unsigned long long now;
+
     if (info->banned) {
         if ((now = rdtsc()) < info->banned_until) {
             unsigned long long banned_time = info->banned_until - now;
@@ -139,133 +136,133 @@ static int __scl_mutex_lock_begin(scl_mutex_t *impl, tinfo_t *info,
         }
     }
 
-    scl_node_t n = {0};
+    scl_node_t cur_qnode = {0};
     while (1) {
-        scl_node_t *prev = impl->tail;
-        if (__sync_bool_compare_and_swap(&impl->tail, prev, &n)) {
-            // enter the lock queue
-            if (!prev) {
-                n.state    = RUNNABLE;
-                impl->next = &n;
-            } else {
-                if (prev == flqnode(impl)) {
-                    n.state    = NEXT;
-                    prev->next = &n;
-                } else {
-                    prev->next = &n;
-                    // wait until we become the next runnable
-#ifdef DEBUG
-                    now = rdtsc();
-#endif
-                    do {
-                        futex((int *)&n.state, FUTEX_WAIT_PRIVATE, INIT, NULL);
-                    } while (INIT == n.state);
-#ifdef DEBUG
-                    info->stat.next_runnable_wait += rdtsc() - now;
-#endif
-                }
-            }
-            // invariant: n.state >= NEXT
-
-            // wait until the current slice expires
-            int slice_valid;
-            unsigned long long curr_slice;
-            while ((slice_valid = impl->slice_valid) &&
-                   (now = rdtsc()) + sleep_granularity <
-                       (curr_slice = impl->slice)) {
-                unsigned long long slice_left = curr_slice - now;
-                struct timespec timeout       = {
-                    .tv_sec = 0, // slice will be less then 1 sec
-                    .tv_nsec =
-                        (slice_left / (CYCLE_PER_US * sleep_granularity)) *
-                        sleep_granularity * 1000,
-                };
-                futex((int *)&impl->slice_valid, FUTEX_WAIT_PRIVATE, 0,
-                      &timeout);
-#ifdef DEBUG
-                info->stat.prev_slice_wait += rdtsc() - now;
-#endif
-            }
-            if (slice_valid) {
-                spin_then_yield(spin_limit, (slice_valid = impl->slice_valid) &&
-                                                rdtsc() < impl->slice);
-                if (slice_valid)
-                    impl->slice_valid = 0;
-            }
-            // invariant: rdtsc() >= curr_slice && lock->slice_valid == 0
-
+        scl_node_t *prev_qnode = lock->tail;
+        if (!__sync_bool_compare_and_swap(&lock->tail, prev_qnode,
+                                          &cur_qnode)) {
+            continue; // lock->tail changed... try again
+        }
+        // cur_qnode entered the lock queue, complete the lock chain
+        if (!prev_qnode) { // no more waiting threads
+            cur_qnode.state = RUNNABLE;
+            lock->next      = &cur_qnode;
+        } else if (prev_qnode == (scl_node_t *)lock) { // next runnable
+            cur_qnode.state  = NEXT;
+            prev_qnode->next = &cur_qnode;
+        } else { // waiters
+            prev_qnode->next = &cur_qnode;
+            // wait until we become the next runnable
 #ifdef DEBUG
             now = rdtsc();
 #endif
-            // spin until RUNNABLE and try to grab the lock
-            spin_then_yield(spin_limit,
-                            RUNNABLE != n.state ||
-                                0 == __sync_bool_compare_and_swap(
-                                         &n.state, RUNNABLE, RUNNING));
-            // invariant: n.state == RUNNING
+            // park till cur_qnode is ready to run
+            do {
+                futex((int *)&cur_qnode.state, FUTEX_WAIT_PRIVATE, INIT, NULL);
+            } while (INIT == cur_qnode.state);
 #ifdef DEBUG
-            info->stat.runnable_wait += rdtsc() - now;
+            info->stat.next_runnable_wait += rdtsc() - now;
 #endif
-
-            // record the successor in the lock so we can notify it when we
-            // release
-            scl_node_t *succ = n.next;
-            if (NULL == succ) {
-                impl->next = NULL;
-                if (0 == __sync_bool_compare_and_swap(&impl->tail, &n,
-                                                      flqnode(impl))) {
-                    spin_then_yield(spin_limit, NULL == (succ = n.next));
-#ifdef DEBUG
-                    info->stat.succ_wait += rdtsc() - now;
-#endif
-                    impl->next = succ;
-                }
-            } else {
-                impl->next = succ;
-            }
-            // invariant: NULL == succ <=> lock->qtail == flqnode(lock)
-
-            now               = rdtsc();
-            info->start_ticks = now;
-            info->slice       = now + FAIRLOCK_GRANULARITY;
-            impl->slice       = info->slice;
-            impl->slice_valid = 1;
-            // wake up successor if necessary
-            if (succ) {
-                succ->state = NEXT;
-                futex((int *)&succ->state, FUTEX_WAKE_PRIVATE, 1, NULL);
-            }
-            return 0;
         }
+        // invariant: cur_qnode.state >= NEXT
+
+        // wait until the current slice expires
+        int slice_valid;
+        unsigned long long curr_slice;
+        while ((slice_valid = lock->slice_valid) &&
+               (now = rdtsc()) + sleep_granularity <
+                   (curr_slice = lock->slice)) {
+            unsigned long long slice_left = curr_slice - now;
+            struct timespec timeout       = {
+                .tv_sec  = 0, // slice will be less then 1 sec
+                .tv_nsec = (slice_left / (CYCLE_PER_US * sleep_granularity)) *
+                           sleep_granularity * 1000,
+            };
+            futex((int *)&lock->slice_valid, FUTEX_WAIT_PRIVATE, 0, &timeout);
+#ifdef DEBUG
+            info->stat.prev_slice_wait += rdtsc() - now;
+#endif
+        }
+
+        if (slice_valid) {
+            spin_then_yield(spin_limit, (slice_valid = lock->slice_valid) &&
+                                            (rdtsc() < lock->slice));
+            lock->slice_valid = 0;
+        }
+        // invariant: rdtsc() >= curr_slice && lock->slice_valid == 0
+
+#ifdef DEBUG
+        now = rdtsc();
+#endif
+        // spin until RUNNABLE and try to grab the lock
+        spin_then_yield(spin_limit,
+                        RUNNABLE != cur_qnode.state ||
+                            0 == __sync_bool_compare_and_swap(
+                                     &cur_qnode.state, RUNNABLE, RUNNING));
+        // invariant: cur_qnode.state == RUNNING
+#ifdef DEBUG
+        info->stat.runnable_wait += rdtsc() - now;
+#endif
+
+        // record the successor in the lock so we can notify it when we release
+        scl_node_t *next_qnode = cur_qnode.next;
+        if (!next_qnode) {
+            lock->next = NULL;
+            if (!__sync_bool_compare_and_swap(&lock->tail, &cur_qnode, lock)) {
+                spin_then_yield(spin_limit, !cur_qnode.next);
+#ifdef DEBUG
+                info->stat.succ_wait += rdtsc() - now;
+#endif
+                next_qnode = cur_qnode.next;
+                lock->next = next_qnode;
+            }
+        } else {
+            lock->next = next_qnode;
+        }
+
+        // invariant: NULL == next_qnode <=> lock->qtail == flqnode(lock)
+        now               = rdtsc();
+        info->start_ticks = now;
+        info->slice       = now + FAIRLOCK_GRANULARITY;
+        lock->slice       = info->slice;
+        lock->slice_valid = 1;
+
+        // wake up successor if necessary
+        if (next_qnode) {
+            next_qnode->state = NEXT;
+            futex((int *)&next_qnode->state, FUTEX_WAKE_PRIVATE, 1, NULL);
+        }
+
+        return 0;
     }
 }
 
-int scl_mutex_lock(scl_mutex_t *impl, scl_node_t *UNUSED(me)) {
+int scl_mutex_lock(scl_mutex_t *lock, scl_node_t *UNUSED(me)) {
     unsigned long long now;
 
-    tinfo_t *info = __thread_info(impl);
+    tinfo_t *info = __thread_info(lock);
 
-    if (impl->slice_valid) {
-        unsigned long long curr_slice = impl->slice;
+    if (lock->slice_valid) {
+        unsigned long long curr_slice = lock->slice;
         // If owner of current slice, try to reenter at the beginning of the
         // queue
         if (curr_slice == info->slice && (now = rdtsc()) < curr_slice) {
-            scl_node_t *succ = impl->next;
+            scl_node_t *succ = lock->next;
             if (!succ) { // next waiting thread is empty
-                if (__sync_bool_compare_and_swap(&impl->tail, NULL,
-                                                 flqnode(impl))) {
+                if (__sync_bool_compare_and_swap(&lock->tail, NULL, lock)) {
+                    // lock is not held, reenter
                     __scl_mutex_lock_reenter(info, now);
                     return 0;
                 }
                 spin_then_yield(spin_limit, (now = rdtsc()) < curr_slice &&
-                                                NULL == (succ = impl->next));
+                                                NULL == (succ = lock->next));
 #ifdef DEBUG
                 info->stat.own_slice_wait += rdtsc() - now;
 #endif
                 // let the succ invalidate the slice, and don't need to wake it
                 // up because slice expires naturally
                 if (now >= curr_slice) {
-                    return __scl_mutex_lock_begin(impl, info, now);
+                    return __scl_mutex_lock(lock, info);
                 }
             }
             // if state < RUNNABLE, it won't become RUNNABLE unless someone
@@ -278,51 +275,60 @@ int scl_mutex_lock(scl_mutex_t *impl, scl_node_t *UNUSED(me)) {
             }
         }
     }
-    return __scl_mutex_lock_begin(impl, info, now);
+    return __scl_mutex_lock(lock, info);
 }
 
-void scl_mutex_unlock(scl_mutex_t *impl, scl_node_t *UNUSED(me)) {
+static void
+__scl_mutex_unlock_accounting(scl_mutex_t *lock,
+                              unsigned long long release_succ_wait) {
     unsigned long long now, cs;
-#ifdef DEBUG
-    unsigned long long succ_start = 0, succ_end = 0;
-#endif
     tinfo_t *info;
 
-    scl_node_t *succ = impl->next;
-    if (!succ) {
-        if (__sync_bool_compare_and_swap(&impl->tail, flqnode(impl), NULL))
-            goto accounting;
-#ifdef DEBUG
-        succ_start = rdtsc();
-#endif
-        spin_then_yield(spin_limit, NULL == (succ = impl->next));
-#ifdef DEBUG
-        succ_end = rdtsc();
-#endif
-    }
-    succ->state = RUNNABLE;
-
-accounting:
     // invariant: NULL == succ || succ->state = RUNNABLE
-    info = (tinfo_t *)pthread_getspecific(impl->tinfo_key);
+    info = (tinfo_t *)pthread_getspecific(lock->tinfo_key);
     now  = rdtsc();
     cs   = now - info->start_ticks;
     info->banned_until +=
         cs *
-        (__atomic_load_n(&impl->total_weight, __ATOMIC_RELAXED) / info->weight);
+        (__atomic_load_n(&lock->total_weight, __ATOMIC_RELAXED) / info->weight);
     info->banned = now < info->banned_until;
 
     if (info->banned) {
-        if (__sync_bool_compare_and_swap(&impl->slice_valid, 1, 0)) {
-            futex((int *)&impl->slice_valid, FUTEX_WAKE_PRIVATE, 1, NULL);
+        if (__sync_bool_compare_and_swap(&lock->slice_valid, 1, 0)) {
+            futex((int *)&lock->slice_valid, FUTEX_WAKE_PRIVATE, 1, NULL);
         }
     }
 #ifdef DEBUG
-    info->stat.release_succ_wait += succ_end - succ_start;
+    info->stat.release_succ_wait += release_succ_wait;
 #endif
 }
 
-int scl_mutex_trylock(scl_mutex_t *impl, scl_node_t *me) {
+void scl_mutex_unlock(scl_mutex_t *lock, scl_node_t *UNUSED(me)) {
+#ifdef DEBUG
+    unsigned long long succ_start = 0, succ_end = 0;
+#endif
+    unsigned long long release_succ_wait = 0;
+
+    scl_node_t *next_qnode = lock->next;
+    if (!next_qnode) {
+        if (__sync_bool_compare_and_swap(&lock->tail, lock, NULL)) {
+            return __scl_mutex_unlock_accounting(lock, release_succ_wait);
+        }
+#ifdef DEBUG
+        succ_start = rdtsc();
+#endif
+        spin_then_yield(spin_limit, !lock->next);
+        next_qnode = lock->next;
+#ifdef DEBUG
+        succ_end          = rdtsc();
+        release_succ_wait = succ_end - succ_start;
+#endif
+    }
+    next_qnode->state = RUNNABLE;
+    return __scl_mutex_unlock_accounting(lock, release_succ_wait);
+}
+
+int scl_mutex_trylock(scl_mutex_t *lock, scl_node_t *me) {
     // The original implementation does not come with a trylock primitive
     // However, one can simply modify the mutex_lock to implement such function
     // if needed...
